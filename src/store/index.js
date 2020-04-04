66660d1d44287ed23bdd3516adf6261b94b9b98d
yuyaamano <nba.chako.nba@icloud.com>
2020-04-04 15:35:04 +0900

    git init

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    drawer: false
+  },
+  mutations: {
+    toggleSideMenu(state){
+      state.drawer = !state.drawer
+    }
+  },
+  actions: {
+    toggleSideMenu({ commit }){
+      commit('toggleSideMenu')
+    }
+  }
+})
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Addresses/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Addresses from '@/components/Addresses.vue'
+export default {
+  name: 'home',
+  components: {
+    Addresses
+  }
+}
+</script>
