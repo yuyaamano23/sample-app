b42a63e5c4e28353e46ee6ecc0dba27f91cfd26e
yuyaamano <nba.chako.nba@icloud.com>
2020-04-05 14:54:04 +0900

    firebaseでログイン機能を実装

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    drawer: false
+    drawer: false,
+    addresses: []
   },
   mutations: {
     toggleSideMenu (state) {
       state.drawer = !state.drawer
+    },
+    addAddress (state, address) {
+      state.addresses.push(address)
     }
   },
   actions: {
+    login () {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(google_auth_provider)
+    },
     toggleSideMenu ({ commit }) {
       commit('toggleSideMenu')
+    },
+    addAddress ({ commit }, address) {
+      commit('addAddress', address)
     }
   }
 })
