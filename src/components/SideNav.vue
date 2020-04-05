dc893b432c50f5d7652f4add014d454785ef994e
yuyaamano <nba.chako.nba@icloud.com>
2020-04-05 18:48:47 +0900

    ログイン状態によるルーティングの制御

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -3,10 +3,10 @@
     <v-list>
       <v-list-item>
         <v-list-item-avatar>
-          <img src="https://avatars2.githubusercontent.com/u/1363954?s=460&v=4">
+          <img v-if="photoURL" :src="photoURL">
         </v-list-item-avatar>
         <v-list-item-content>
-          <v-list-item-title>Kazuya Kojima</v-list-item-title>
+          <v-list-item-title>{{ userName }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
@@ -25,14 +25,17 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   data () {
     return {
       items: [
-        { title: 'ホーム', icon: 'mdi-home', link: { name: 'home'} },
         { title: '連絡先一覧', icon: 'mdi-menu', link: { name: 'addresses' } }
       ]
     }
+  },
+  computed: {
+    ...mapGetters(['userName', 'photoURL'])
   }
 }
 </script>
